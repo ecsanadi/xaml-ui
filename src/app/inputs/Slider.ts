7833a87283daeff6795b249fb7ceaece3d110447
Peter Major <peter.major@cae.com>
2025-04-17 12:21:21 +0200

    Remove unneeded import

--- src/app/inputs/Slider.ts
+++ src/app/inputs/Slider.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostBinding, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { FrameworkElement } from "../layout/FrameworkElement";
 import { Orientation } from "../layout/Common";
 
