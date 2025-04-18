5230a9373c85672d236a64c9b74dd624f49e3cbb
Peter Major <axodox@outlook.com>
2025-04-18 19:19:30 +0200

    WIP

--- projects/xaml-sandbox/src/app/app.component.ts
+++ projects/xaml-sandbox/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { XamlRootComponent } from '../../../xaml-ui/src/public-api';
 import { StackPanelComponent } from "../../../xaml-ui/src/lib/layout/StackPanel";
 import { ButtonComponent } from "../../../xaml-ui/src/lib/basic-input/Button";
 import { Flyout } from "../../../xaml-ui/src/lib/dialogs-and-flyouts/Flyout";
+import { ListBoxComponent } from "../../../xaml-ui/src/lib/collections/ListBox";
+import { ScrollViewerComponent } from "../../../xaml-ui/src/lib/scrolling/ScrollViewer";
 
 @Component({
   selector: 'app-root',
-  imports: [XamlRootComponent, StackPanelComponent, ButtonComponent, Flyout],
+  imports: [XamlRootComponent, StackPanelComponent, ButtonComponent, Flyout, ListBoxComponent, ScrollViewerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- projects/xaml-ui/src/lib/scrolling/ScrollViewer.ts
+++ projects/xaml-ui/src/lib/scrolling/ScrollViewer.ts
@@ -1,18 +1,77 @@
 import { CommonModule } from "@angular/common";
 import { FrameworkElementComponent } from "../FrameworkElement";
-import { Component, HostBinding, Input } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, HostBinding, HostListener, Input, OnInit, ViewChild } from "@angular/core";
 import { ScrollMode } from "../Common";
 
 @Component({
   selector: 'ScrollViewer',
-  template: `<ng-content/>`,
-  styles: `:host {
-    display: grid;
-  }`
+  imports: [CommonModule],
+  template: `<div #content class="content" [ngStyle]="contentStyle" (scroll)="updateScrollPosition()"><ng-content/></div>
+  <div #scrollbar class="scrollbar"><div #thumb class="thumb" (pointerdown)="onPointerDown($event)" (pointermove)="onPointerMove($event)" (pointerup)="onPointerUp($event)"></div></div>`,
+  styleUrl: 'ScrollViewer.scss'
 })
-export class ScrollViewerComponent extends FrameworkElementComponent {
-  @Input() HorizontalScrollMode: ScrollMode = 'Enabled';
+export class ScrollViewerComponent extends FrameworkElementComponent implements AfterViewInit {
   @Input() VerticalScrollMode: ScrollMode = 'Enabled';
+
+  @ViewChild('content')
+  private _content!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('scrollbar')
+  private _scrollbar!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('thumb')
+  private _thumb!: ElementRef<HTMLDivElement>;
+
+  private _isScrolling = false;
+  private _startPointerY = 0;
+  private _startScrollY = 0;
+
+  ngAfterViewInit(): void {
+    this.updateScrollPosition();
+  }
+
+  updateScrollPosition() {
+    let content = this._content.nativeElement;
+    let scrollbar = this._scrollbar.nativeElement;
+    let thumb = this._thumb.nativeElement;
+
+    let scrollPercent = content.scrollTop / (content.scrollHeight - content.clientHeight);
+    let thumbHeight = scrollbar.offsetHeight * (content.clientHeight / content.scrollHeight);
+    thumb.style.height = `${thumbHeight}px`;
+    thumb.style.top = `${scrollPercent * (scrollbar.offsetHeight - thumbHeight)}px`;
+  }
+
+  onPointerDown(event: PointerEvent) {
+    if (event.buttons !== 1) return;
+
+    this._thumb.nativeElement.setPointerCapture(event.pointerId);
+    this._isScrolling = true;
+    this._startPointerY = event.clientY;
+    this._startScrollY = this._content.nativeElement.scrollTop;
+  }
+
+  onPointerMove(event: PointerEvent) {
+    if (!this._isScrolling) return;
+    if (event.buttons !== 1) {
+      this._isScrolling = false;
+      return;
+    }
+
+    let content = this._content.nativeElement;
+    let scrollbar = this._scrollbar.nativeElement;
+    let thumb = this._thumb.nativeElement;
+    let deltaY = event.clientY - this._startPointerY;
+
+    const scrollPercent = deltaY / (scrollbar.offsetHeight - thumb.offsetHeight);
+    content.scrollTop = this._startScrollY + scrollPercent * (content.scrollHeight - content.clientHeight);
+  }
+
+  onPointerUp(event: PointerEvent) {
+    if (event.buttons !== 1) return;
+
+    this._isScrolling = false;
+    this._thumb.nativeElement.releasePointerCapture(event.pointerId);
+  }
 
   private static toOverflow(value: ScrollMode) {
     switch (value) {
@@ -25,13 +84,9 @@ export class ScrollViewerComponent extends FrameworkElementComponent {
     }
   }
 
-  @HostBinding('style.overflow-x')
-  private get overflowX() {
-    return ScrollViewerComponent.toOverflow(this.HorizontalScrollMode);
-  }
-
-  @HostBinding('style.overflow-y')
-  private get overflowY() {
-    return ScrollViewerComponent.toOverflow(this.VerticalScrollMode);
+  get contentStyle() {
+    return {
+      'overflow-y': ScrollViewerComponent.toOverflow(this.VerticalScrollMode)
+    };
   }
 }
