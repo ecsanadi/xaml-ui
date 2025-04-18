9a81f464498ca08e09cc1ac37e2c68c2fc7a4273
Peter Major <axodox@outlook.com>
2025-04-18 21:52:42 +0200

    Use css vars for theming

--- projects/xaml-sandbox/src/app/app.component.ts
+++ projects/xaml-sandbox/src/app/app.component.ts
@@ -6,10 +6,15 @@ import { Flyout } from "../../../xaml-ui/src/lib/dialogs-and-flyouts/Flyout";
 import { ListBoxComponent } from "../../../xaml-ui/src/lib/collections/ListBox";
 import { ScrollViewerComponent } from "../../../xaml-ui/src/lib/scrolling/ScrollViewer";
 import { ScrollBar } from "../../../xaml-ui/src/lib/scrolling/ScrollBar";
+import { CheckBoxComponent } from "../../../xaml-ui/src/lib/basic-input/CheckBox";
+import { RadioButtonComponent } from "../../../xaml-ui/src/lib/basic-input/RadioButton";
+import { SliderComponent } from "../../../xaml-ui/src/lib/basic-input/Slider";
+import { ToggleSwitchComponent } from "../../../xaml-ui/src/lib/basic-input/ToggleSwitch";
+import { TextBoxComponent } from "../../../xaml-ui/src/lib/text/TextBox";
 
 @Component({
   selector: 'app-root',
-  imports: [XamlRootComponent, StackPanelComponent, ButtonComponent, Flyout, ListBoxComponent, ScrollViewerComponent, ScrollBar],
+  imports: [XamlRootComponent, StackPanelComponent, ButtonComponent, Flyout, ListBoxComponent, ScrollViewerComponent, ScrollBar, CheckBoxComponent, RadioButtonComponent, SliderComponent, ToggleSwitchComponent, TextBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
