1a68b0e4f5bfacf831e9425a88218c702505ced8
Peter Major <axodox@outlook.com>
2025-06-09 18:07:53 +0200

    Move XamlRoot template to separate file

--- projects/xaml-ui/src/lib/XamlRoot.ts
+++ projects/xaml-ui/src/lib/XamlRoot.ts
@@ -3,22 +3,7 @@ import { Component, HostListener } from "@angular/core";
 @Component({
   selector: 'XamlRoot',
   template: `<ng-content/>`,
-  styles: `:host { 
-    display: grid;    
-    overflow: hidden;
-    user-select: none; 
-    background-color: #f3f3f3;
-    touch-action: none;
-
-    &::ng-deep * {
-      user-select: none;
-      touch-action: none;      
-    }
-
-    @media (prefers-color-scheme: dark) {
-      background-color: #202020;
-    }
-  }`
+  styleUrl: 'XamlRoot.scss'
 })
 export class XamlRootComponent {
 
