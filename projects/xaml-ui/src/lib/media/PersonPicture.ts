e9708ea92fa5e6387ad49958470d5086f96070eb
Enikő Csanádi <ecsanadi@cae.com>
2025-08-18 11:23:13 +0200

    Implement PersonPicture component

---
+++ projects/xaml-ui/src/lib/media/PersonPicture.ts
@@ -0,0 +1,45 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { FrameworkElementComponent } from "../FrameworkElement";
+
+@Component({
+  selector: 'PersonPicture',
+  template: `<div class="person-picture">
+  <div class="avatar">
+    <img *ngIf="ProfilePicture; else initialsTemplate"
+         [src]="ProfilePicture" alt="{{ DisplayName || Initials }}" />
+    <ng-template #initialsTemplate>
+      <span class="initials">{{ Initials || fallbackInitials() }}</span>
+    </ng-template>
+  </div>
+  <div *ngIf="BadgeText || BadgeGlyph || BadgeImageSrc" class="badge">
+    <img *ngIf="BadgeImageSrc" [src]="BadgeImageSrc" />
+    <span *ngIf="BadgeText">{{ BadgeText }}</span>
+    <span *ngIf="BadgeGlyph" class="glyph">{{ BadgeGlyph }}</span>
+  </div>
+</div>`,
+  styleUrl: 'PersonPicture.scss',
+  imports: [CommonModule]
+})
+export class PersonPictureComponent extends FrameworkElementComponent{
+  @Input() @HostBinding('style.color') Foreground?: string;
+
+  @Input() DisplayName?: string;
+  @Input() ProfilePicture?: string;
+  @Input() Initials?: string;
+  @Input() BadgeText?: string;
+  @Input() BadgeGlyph?: string;
+  @Input() BadgeImageSrc?: string;
+
+  fallbackInitials(): string {
+    if (this.DisplayName) {
+      return this.DisplayName
+        .split(' ')
+        .map(n => n[0])
+        .join('')
+        .substring(0, 2)
+        .toUpperCase();
+    }
+    return '?';
+  }
+}
